42d09ccfd8c0869bb29046ce7c8c842dd4e0047c
TM-khoa <minhkhoa9a31314@gmail.com>
2024-07-13 22:49:22 +0700

    feat:reduce HAL overhead API, reduce render time

--- FATFS/App/fatfs_sd.c
+++ FATFS/App/fatfs_sd.c
@@ -22,38 +22,269 @@ static uint8_t PowerFlag = 0; /* Power flag */
  * SPI functions
  **************************************/
 
+static HAL_StatusTypeDef SPI_WaitFlagStateUntilTimeoutOptimize(SPI_HandleTypeDef *hspi, uint32_t Flag, FlagStatus State, uint32_t Timeout, uint32_t Tickstart) {
+	__IO uint32_t count;
+	uint32_t tmp_timeout;
+	uint32_t tmp_tickstart;
+
+	/* Adjust Timeout value  in case of end of transfer */
+	tmp_timeout = Timeout - (HAL_GetTick() - Tickstart);
+	tmp_tickstart = HAL_GetTick();
+
+	/* Calculate Timeout based on a software loop to avoid blocking issue if Systick is disabled */
+	count = tmp_timeout * ((SystemCoreClock * 32U) >> 20U);
+
+	while ((__HAL_SPI_GET_FLAG(hspi, Flag) ? SET : RESET) != State) {
+		if (Timeout != HAL_MAX_DELAY) {
+			if (((HAL_GetTick() - tmp_tickstart) >= tmp_timeout) || (tmp_timeout == 0U)) {
+				/* Disable the SPI and reset the CRC: the CRC value should be cleared
+				 on both master and slave sides in order to resynchronize the master
+				 and slave for their respective CRC calculation */
+
+				/* Disable TXE, RXNE and ERR interrupts for the interrupt process */
+				__HAL_SPI_DISABLE_IT(hspi, (SPI_IT_TXE | SPI_IT_RXNE | SPI_IT_ERR));
+
+				if ((hspi->Init.Mode == SPI_MODE_MASTER) && ((hspi->Init.Direction == SPI_DIRECTION_1LINE) || (hspi->Init.Direction == SPI_DIRECTION_2LINES_RXONLY))) {
+					/* Disable SPI peripheral */
+					__HAL_SPI_DISABLE(hspi);
+				}
+				hspi->State = HAL_SPI_STATE_READY;
+
+				/* Process Unlocked */
+				__HAL_UNLOCK(hspi);
+
+				return HAL_TIMEOUT;
+			}
+			/* If Systick is disabled or not incremented, deactivate timeout to go in disable loop procedure */
+			if (count == 0U) {
+				tmp_timeout = 0U;
+			}
+			count--;
+		}
+	}
+
+	return HAL_OK;
+}
+
+static HAL_StatusTypeDef SPI_EndRxTxTransactionOptimize(SPI_HandleTypeDef *hspi, uint32_t Timeout, uint32_t Tickstart) {
+	/* Wait until TXE flag */
+	while ((SPI1->SR & SPI_SR_TXE) != SPI_SR_TXE); // keep in loop if TXE not set
+	while ((SPI1->SR & SPI_SR_BSY) == SPI_SR_BSY); // keep in loop if BSY is set
+//	(__HAL_SPI_GET_FLAG(hspi, SPI_FLAG_TXE) ? SET : RESET) != SET)
+//	if (SPI_WaitFlagStateUntilTimeoutOptimize(hspi, SPI_FLAG_TXE, SET, Timeout, Tickstart) != HAL_OK) {
+//		SET_BIT(hspi->ErrorCode, HAL_SPI_ERROR_FLAG);
+//		return HAL_TIMEOUT;
+//	}
+//	if (hspi->Init.Mode == SPI_MODE_MASTER) {
+//		/* Control the BSY flag */
+//		if (SPI_WaitFlagStateUntilTimeoutOptimize(hspi, SPI_FLAG_BSY, RESET, Timeout, Tickstart) != HAL_OK) {
+//			SET_BIT(hspi->ErrorCode, HAL_SPI_ERROR_FLAG);
+//			return HAL_TIMEOUT;
+//		}
+//	}
+	return HAL_OK;
+}
+
+HAL_StatusTypeDef SPI_TransmitOptimize(SPI_HandleTypeDef *hspi, uint8_t *pData, uint16_t Size, uint32_t Timeout) {
+	uint32_t tickstart;
+	HAL_StatusTypeDef errorcode = HAL_OK;
+	uint16_t initial_TxXferCount;
+
+	/* Process Locked */
+	__HAL_LOCK(hspi);
+
+	/* Init tickstart for timeout management*/
+	tickstart = HAL_GetTick();
+	initial_TxXferCount = Size;
+
+	if (hspi->State != HAL_SPI_STATE_READY) {
+		errorcode = HAL_BUSY;
+		goto error;
+	}
+
+	if ((pData == NULL) || (Size == 0U)) {
+		errorcode = HAL_ERROR;
+		goto error;
+	}
+
+	/* Set the transaction information */
+	hspi->State = HAL_SPI_STATE_BUSY_TX;
+	hspi->ErrorCode = HAL_SPI_ERROR_NONE;
+	hspi->pTxBuffPtr = (uint8_t*) pData;
+	hspi->TxXferSize = Size;
+	hspi->TxXferCount = Size;
+
+	SPI1->CR1 |= SPI_CR1_SPE;
+
+	if (initial_TxXferCount == 0x01U) {
+		*((__IO uint8_t*) &hspi->Instance->DR) = (*hspi->pTxBuffPtr);
+		hspi->pTxBuffPtr += sizeof(uint8_t);
+		hspi->TxXferCount--;
+	}
+	while (hspi->TxXferCount > 0U) {
+		/* Wait until TXE flag is set to send data */
+		if (__HAL_SPI_GET_FLAG(hspi, SPI_FLAG_TXE)) {
+			*((__IO uint8_t*) &hspi->Instance->DR) = (*hspi->pTxBuffPtr);
+			hspi->pTxBuffPtr += sizeof(uint8_t);
+			hspi->TxXferCount--;
+		}
+		else {
+			/* Timeout management */
+			if ((((HAL_GetTick() - tickstart) >= Timeout) && (Timeout != HAL_MAX_DELAY)) || (Timeout == 0U)) {
+				errorcode = HAL_TIMEOUT;
+				hspi->State = HAL_SPI_STATE_READY;
+				goto error;
+			}
+		}
+	}
+
+	while ((SPI1->SR & SPI_SR_TXE) != SPI_SR_TXE); // keep in loop if TXE not set
+	while ((SPI1->SR & SPI_SR_BSY) == SPI_SR_BSY); // keep in loop if BSY is set
+
+	/* Clear overrun flag in 2 Lines communication mode because received is not read */
+	__HAL_SPI_CLEAR_OVRFLAG(hspi);
+	if (hspi->ErrorCode != HAL_SPI_ERROR_NONE) {
+		errorcode = HAL_ERROR;
+	}
+	else {
+		hspi->State = HAL_SPI_STATE_READY;
+	}
+
+	error:
+	/* Process Unlocked */
+	__HAL_UNLOCK(hspi);
+	return errorcode;
+}
+
+HAL_StatusTypeDef SPI_TransmitReceiveOptimize(SPI_HandleTypeDef *hspi, uint8_t *pTxData, uint8_t *pRxData, uint16_t Size, uint32_t Timeout) {
+	uint16_t initial_TxXferCount;
+	uint32_t tmp_mode;
+	HAL_SPI_StateTypeDef tmp_state;
+	uint32_t tickstart;
+
+	/* Variable used to alternate Rx and Tx during transfer */
+	uint32_t txallowed = 1U;
+	HAL_StatusTypeDef errorcode = HAL_OK;
+
+	/* Process Locked */
+	__HAL_LOCK(hspi);
+
+	/* Init tickstart for timeout management*/
+	tickstart = HAL_GetTick();
+
+	/* Init temporary variables */
+	tmp_state = hspi->State;
+	tmp_mode = hspi->Init.Mode;
+	initial_TxXferCount = Size;
+
+	if (!((tmp_state == HAL_SPI_STATE_READY) || ((tmp_mode == SPI_MODE_MASTER) && (hspi->Init.Direction == SPI_DIRECTION_2LINES) && (tmp_state == HAL_SPI_STATE_BUSY_RX)))) {
+		errorcode = HAL_BUSY;
+		goto error;
+	}
+
+	if ((pTxData == NULL) || (pRxData == NULL) || (Size == 0U)) {
+		errorcode = HAL_ERROR;
+		goto error;
+	}
+
+	/* Set the transaction information */
+	hspi->ErrorCode = HAL_SPI_ERROR_NONE;
+	hspi->pRxBuffPtr = (uint8_t*) pRxData;
+	hspi->RxXferCount = Size;
+	hspi->RxXferSize = Size;
+	hspi->pTxBuffPtr = (uint8_t*) pTxData;
+	hspi->TxXferCount = Size;
+	hspi->TxXferSize = Size;
+
+	SPI1->CR1 |= SPI_CR1_SPE;
+
+	/* Transmit and Receive data in 8 Bit mode */
+	if (initial_TxXferCount == 0x01U) {
+		*((__IO uint8_t*) &hspi->Instance->DR) = (*hspi->pTxBuffPtr);
+		hspi->pTxBuffPtr += sizeof(uint8_t);
+		hspi->TxXferCount--;
+	}
+	while ((hspi->TxXferCount > 0U) || (hspi->RxXferCount > 0U)) {
+		/* Check TXE flag */
+		if ((__HAL_SPI_GET_FLAG(hspi, SPI_FLAG_TXE)) && (hspi->TxXferCount > 0U) && (txallowed == 1U)) {
+			*(__IO uint8_t*) &hspi->Instance->DR = (*hspi->pTxBuffPtr);
+			hspi->pTxBuffPtr++;
+			hspi->TxXferCount--;
+			/* Next Data is a reception (Rx). Tx not allowed */
+			txallowed = 0U;
+		}
+		/* Wait until RXNE flag is reset */
+		if ((__HAL_SPI_GET_FLAG(hspi, SPI_FLAG_RXNE)) && (hspi->RxXferCount > 0U)) {
+			(*(uint8_t*) hspi->pRxBuffPtr) = hspi->Instance->DR;
+			hspi->pRxBuffPtr++;
+			hspi->RxXferCount--;
+			/* Next Data is a Transmission (Tx). Tx is allowed */
+			txallowed = 1U;
+		}
+		if ((((HAL_GetTick() - tickstart) >= Timeout) && ((Timeout != HAL_MAX_DELAY))) || (Timeout == 0U)) {
+			errorcode = HAL_TIMEOUT;
+			hspi->State = HAL_SPI_STATE_READY;
+			goto error;
+		}
+	}
+
+	while ((SPI1->SR & SPI_SR_TXE) != SPI_SR_TXE); // keep in loop if TXE not set
+	while ((SPI1->SR & SPI_SR_BSY) == SPI_SR_BSY); // keep in loop if BSY is set
+
+	/* Clear overrun flag in 2 Lines communication mode because received is not read */
+
+	__HAL_SPI_CLEAR_OVRFLAG(hspi);
+
+	if (hspi->ErrorCode != HAL_SPI_ERROR_NONE) {
+		errorcode = HAL_ERROR;
+	}
+	else {
+		hspi->State = HAL_SPI_STATE_READY;
+	}
+
+	error:
+	__HAL_UNLOCK(hspi);
+	return errorcode;
+}
 /* slave select */
 static void SELECT(void) {
-	HAL_GPIO_WritePin(SD_CS_PORT, SD_CS_PIN, GPIO_PIN_RESET);
-	HAL_Delay(1);
+//	HAL_GPIO_WritePin(SD_CS_PORT, SD_CS_PIN, GPIO_PIN_RESET);
+	GPIOB->BSRR = SD_CS_PIN << 16;
+	__NOP();
+	__NOP();
+	__NOP();
+//	HAL_Delay(1);
 }
 
 /* slave deselect */
 static void DESELECT(void) {
-	HAL_GPIO_WritePin(SD_CS_PORT, SD_CS_PIN, GPIO_PIN_SET);
-	HAL_Delay(1);
+//	HAL_GPIO_WritePin(SD_CS_PORT, SD_CS_PIN, GPIO_PIN_SET);
+	GPIOB->BSRR = SD_CS_PIN;
+	__NOP();
+	__NOP();
+	__NOP();
+//	HAL_Delay(1);
 }
 
 /* SPI transmit a byte */
 static void SPI_TxByte(uint8_t data) {
 	while (!__HAL_SPI_GET_FLAG(HSPI_SDCARD, SPI_FLAG_TXE));
-	HAL_SPI_Transmit(HSPI_SDCARD, &data, 1, SPI_TIMEOUT);
+	SPI_TransmitOptimize(HSPI_SDCARD, &data, 1, SPI_TIMEOUT);
 }
 
 /* SPI transmit buffer */
 static void SPI_TxBuffer(uint8_t *buffer, uint16_t len) {
 	while (!__HAL_SPI_GET_FLAG(HSPI_SDCARD, SPI_FLAG_TXE));
-	HAL_SPI_Transmit(HSPI_SDCARD, buffer, len, SPI_TIMEOUT);
+	SPI_TransmitOptimize(HSPI_SDCARD, buffer, len, SPI_TIMEOUT);
 }
 
 /* SPI receive a byte */
 static uint8_t SPI_RxByte(void) {
-	uint8_t dummy, data;
+	uint8_t dummy, data = 0;
 	dummy = 0xFF;
 
 	while (!__HAL_SPI_GET_FLAG(HSPI_SDCARD, SPI_FLAG_TXE));
-	HAL_SPI_TransmitReceive(HSPI_SDCARD, &dummy, &data, 1, SPI_TIMEOUT);
-
+	SPI_TransmitReceiveOptimize(HSPI_SDCARD, &dummy, &data, 1, SPI_TIMEOUT);
 	return data;
 }
 
